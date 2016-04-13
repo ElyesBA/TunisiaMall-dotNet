32d16b2979b08f185e41f20eef52ec2abd5bd7a1
Elyes <elyes.benabdelkader@esprit.tn>
2016-04-13 05:31:16 +0100

    soufiene merge

--- TunisiaMallWeb/Controllers/UserController.cs
+++ TunisiaMallWeb/Controllers/UserController.cs
@@ -14,6 +14,145 @@ namespace TunisiaMallWeb.Controllers
 {
     public class UserController : Controller
     {
+        IUserService u = new UserService();
+        IFriendShipService fs = new FriendShipService();
+        // [Authorize]
+        [Route("GetProfile")]
+        public ActionResult GetProfile()
+        {
+
+            user us = CurrentUser.get();
+
+            return View(us);
+        }
+
+
+        // GET: User/Create
+        [HttpGet]
+        [Route("UserCreate")]
+        public ActionResult UserCreate()
+        {
+            user us = new user();
+
+            // string userId = Membership.GetUser().ProviderUserKey.ToString();
+            //gestbookE.user_idUser = 2;
+
+            return View(us);
+        }
+
+
+        [HttpPost]
+        [Route("UserCreate")]
+        public ActionResult UserCreate(user us)
+        {
+            u.Create(us);
+            u.Commit();
+            return Redirect("UserCreate");
+
+        }
+
+
+
+
+        // GET: User/Edit/5
+        [HttpGet]
+        [Route("EditUser")]
+        public ActionResult EditUser()
+        {
+            user us = CurrentUser.get();
+
+            if (us == null)
+            {
+                return HttpNotFound();
+            }
+            return View(us);
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [Route("EditUser")]
+        public ActionResult EditUser(user us)
+        {
+            try
+            {
+                var ur = u.FindById(us.idUser);
+                ur.firstName = us.firstName;
+                ur.birthdate = us.birthdate;
+                ur.job = us.job;
+                ur.mail = us.mail;
+                ur.phone = us.phone;
+                ur.login = us.login;
+                ur.address = us.address;
+                u.Update(ur);
+                u.Commit();
+
+                return Redirect("EditUser");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: User/Delete/5
+        [HttpGet]
+        [Route("DeleteUser")]
+        public ActionResult DeleteUser(int id = 1)
+        {
+            user e = u.FindById(id);
+            return View(e);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        [Route("DeleteUser")]
+        public ActionResult DeleteUser(user us)
+        {
+            user ur = u.FindById(us.idUser);
+            u.Delete(ur);
+            u.Commit();
+
+            return RedirectToAction("DeleteUser");
+        }
+
+
+        [HttpGet]
+        [Route("FriendShip")]
+        public ActionResult FriendShip()
+        {
+
+            IEnumerable<user> users = u.GetMany();
+            List<user> listusers = users.ToList();
+            IEnumerable<friendship> friends = fs.GetMany();
+            List<friendship> listfriends = friends.ToList();
+            ViewBag.lfriends = listfriends;
+            return View(listusers);
+
+        }
+
+        [HttpPost]
+        [Route("addFriend")]
+        public ActionResult addFriend(int idUser1, int idUser2)
+        {
+            try
+            {
+                friendship fr = new friendship();
+                fr.idUser1 = idUser1;
+                fr.idUser2 = idUser2;
+                fr.accepted = true;
+                fs.Create(fr);
+                fs.Commit();
+
+                return RedirectToAction("FriendShip");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
         [HttpPost]
         public ActionResult Login(string username, string password)
         {
@@ -47,11 +186,11 @@ namespace TunisiaMallWeb.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        
+
         public string MySecretAction()
         {
             user user = CurrentUser.get();
-            return  "";
+            return "";
         }
     }
 }
